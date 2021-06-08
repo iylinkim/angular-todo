c0b02fbd901413b64a77573ece39d0f101c1c5d0
iylinkim <kiylin94@naver.com>
2021-06-08 15:11:49 +0900

    Component Input data and Create an event & event emitter output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html', //directory of html file which is currently using
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-crash';
 }
